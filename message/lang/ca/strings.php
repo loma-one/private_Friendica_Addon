499dd3b3634effdd40d28f8421d1a02f49e1c476
loma-one <44441246+loma-one@users.noreply.github.com>
2026-03-31 19:08:56 +0200

    Delete message directory

--- message/lang/ca/strings.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if(! function_exists("string_plural_select_ca")) {
-function string_plural_select_ca($n){
-	$n = intval($n);
-	return intval($n != 1);
-}}
-$a->strings['Report Bug'] = 'Informar d’error';
--- message/lang/da-dk/strings.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if(! function_exists("string_plural_select_da_dk")) {
-function string_plural_select_da_dk($n){
-	$n = intval($n);
-	return intval($n != 1);
-}}
-$a->strings['Report Bug'] = 'Rapportér fejl';
