cf77af85d95e84b0f7afc7a98bacf3332e337831
loma-one <44441246+loma-one@users.noreply.github.com>
2026-07-26 12:14:32 +0200

    Update timelinefilter.php
    Kleine Fehler behoben

--- timelinefilter/timelinefilter.php
+++ timelinefilter/timelinefilter.php
@@ -3,7 +3,7 @@
 /**
  * Name: Timeline Filter
  * Description: Filters hashtags and words in personal timelines
- * Version: 1.3
+ * Version: 1.4
  * Author: Matthias Ebers <https://loma.ml/profile/feb>
  */
 
@@ -13,222 +13,216 @@ use Friendica\DI;
 
 function timelinefilter_install()
 {
-    Hook::register('page_end', 'addon/timelinefilter/timelinefilter.php', 'timelinefilter_page_end', 999);
-    Hook::register('addon_settings', 'addon/timelinefilter/timelinefilter.php', 'timelinefilter_addon_settings');
-    Hook::register('addon_settings_post', 'addon/timelinefilter/timelinefilter.php', 'timelinefilter_addon_settings_post');
+	Hook::register('page_end', 'addon/timelinefilter/timelinefilter.php', 'timelinefilter_page_end', 999);
+	Hook::register('addon_settings', 'addon/timelinefilter/timelinefilter.php', 'timelinefilter_addon_settings');
+	Hook::register('addon_settings_post', 'addon/timelinefilter/timelinefilter.php', 'timelinefilter_addon_settings_post');
 }
 
-function timelinefilter_addon_settings(array &$data)
+function timelinefilter_addon_settings(&$data)
 {
-    if (!DI::userSession()->getLocalUserId()) {
-        return;
-    }
+	$uid = DI::userSession()->getLocalUserId();
+	if (!$uid) {
+		return;
+	}
 
-    $uid = DI::userSession()->getLocalUserId();
-    $enabled = !DI::pConfig()->get($uid, 'timelinefilter', 'disable', 1);
+	$enabled = !DI::pConfig()->get($uid, 'timelinefilter', 'disable', 1);
+	$rules = timelinefilter_get_rules($uid);
 
-    $rules_json = DI::pConfig()->get($uid, 'timelinefilter', 'rules', '[]');
-    $rules = json_decode($rules_json, true) ?: [];
+	if (empty($rules)) {
+		$rules[] = ['keyword' => '', 'type' => 'hashtag', 'duration' => 'always', 'expires' => 0];
+	}
 
-    $current_time = time();
-    $filtered_rules = [];
-    $has_changed = false;
+	$t = Renderer::getMarkupTemplate('settings.tpl', 'addon/timelinefilter/');
+	$html = Renderer::replaceMacros($t, [
+		'$info'        => DI::l10n()->t('Safe Filter: Define personal rules with optional expiration dates to hide posts.'),
+		'$enabled'     => ['timelinefilter-enable', DI::l10n()->t('Enable Filter'), $enabled],
+		'$words_label' => DI::l10n()->t('Filter Rules'),
+		'$words_help'  => DI::l10n()->t('Add keywords, select type and specify how long the filter should remain active.'),
+		'$rules'       => $rules,
+		'$submit'      => DI::l10n()->t('Save Settings')
+	]);
 
-    foreach ($rules as $rule) {
-        if (isset($rule['expires']) && $rule['expires'] > 0 && $current_time > $rule['expires']) {
-            $has_changed = true;
-            continue;
-        }
-        $filtered_rules[] = $rule;
-    }
-
-    if ($has_changed) {
-        DI::pConfig()->set($uid, 'timelinefilter', 'rules', json_encode($filtered_rules));
-        $rules = $filtered_rules;
-    }
-
-    if (empty($rules)) {
-        $rules[] = ['keyword' => '', 'type' => 'hashtag', 'duration' => 'always', 'expires' => 0];
-    }
-
-    $t    = Renderer::getMarkupTemplate('settings.tpl', 'addon/timelinefilter/');
-    $html = Renderer::replaceMacros($t, [
-        '$info'        => DI::l10n()->t('Safe Filter: Define personal rules with optional expiration dates to hide posts.'),
-        '$enabled'     => ['timelinefilter-enable', DI::l10n()->t('Enable Filter'), $enabled],
-        '$words_label' => DI::l10n()->t('Filter Rules'),
-        '$words_help'  => DI::l10n()->t('Add keywords, select type and specify how long the filter should remain active.'),
-        '$rules'       => $rules,
-        '$submit'      => DI::l10n()->t('Save Settings')
-    ]);
-
-    $data = [
-        'addon' => 'timelinefilter',
-        'title' => DI::l10n()->t('Timeline Filter'),
-        'html'  => $html,
-    ];
+	$data = [
+		'addon' => 'timelinefilter',
+		'title' => DI::l10n()->t('Timeline Filter'),
+		'html'  => $html,
+	];
 }
 
-function timelinefilter_addon_settings_post(array &$b)
+function timelinefilter_addon_settings_post(&$b)
 {
-    if (!DI::userSession()->getLocalUserId()) {
-        return;
-    }
+	$uid = DI::userSession()->getLocalUserId();
+	if (!$uid || empty($_POST['timelinefilter-submit'])) {
+		return;
+	}
 
-    if (!empty($_POST['timelinefilter-submit'])) {
-        $uid = DI::userSession()->getLocalUserId();
-        $enable = !empty($_POST['timelinefilter-enable']) ? intval($_POST['timelinefilter-enable']) : 0;
-        $disable = 1 - $enable;
+	$enable = isset($_POST['timelinefilter-enable']) ? intval($_POST['timelinefilter-enable']) : 0;
+	$disable = $enable ? 0 : 1;
 
-        $keywords  = $_POST['tf-keywords'] ?? [];
-        $types     = $_POST['tf-types'] ?? [];
-        $durations = $_POST['tf-durations'] ?? [];
-        $expires   = $_POST['tf-expires'] ?? [];
+	$keywords  = $_POST['tf-keywords'] ?? [];
+	$types     = $_POST['tf-types'] ?? [];
+	$durations = $_POST['tf-durations'] ?? [];
+	$expires   = $_POST['tf-expires'] ?? [];
 
-        $rules = [];
-        $current_time = time();
+	$rules = [];
+	$now = time();
 
-        foreach ($keywords as $index => $keyword) {
-            // trim() entfernt Leerzeichen am Anfang und Ende
-            $keyword = trim($keyword);
-            if (!empty($keyword)) {
-                $duration = $durations[$index] ?? 'always';
-                $expire_timestamp = intval($expires[$index] ?? 0);
+	foreach ($keywords as $i => $kw) {
+		$kw = trim($kw);
+		if ($kw === '') {
+			continue;
+		}
 
-                if ($expire_timestamp == 0) {
-                    if ($duration === '1w') {
-                        $expire_timestamp = $current_time + (7 * 24 * 60 * 60);
-                    } elseif ($duration === '1m') {
-                        $expire_timestamp = $current_time + (30 * 24 * 60 * 60);
-                    } else {
-                        $expire_timestamp = 0; // "Immer"
-                    }
-                }
+		$duration = $durations[$i] ?? 'always';
+		$exp = intval($expires[$i] ?? 0);
 
-                $rules[] = [
-                    'keyword'  => $keyword,
-                    'type'     => $types[$index] ?? 'hashtag',
-                    'duration' => $duration,
-                    'expires'  => $expire_timestamp
-                ];
-            }
-        }
+		if ($exp === 0) {
+			if ($duration === '1w') {
+				$exp = $now + (7 * 86400);
+			} elseif ($duration === '1m') {
+				$exp = $now + (30 * 86400);
+			}
+		}
 
-        DI::pConfig()->set($uid, 'timelinefilter', 'rules', json_encode($rules));
-        DI::pConfig()->set($uid, 'timelinefilter', 'disable', $disable);
-    }
+		$rules[] = [
+			'keyword'  => $kw,
+			'type'     => $types[$i] ?? 'hashtag',
+			'duration' => $duration,
+			'expires'  => $exp,
+		];
+	}
+
+	DI::pConfig()->set($uid, 'timelinefilter', 'rules', json_encode($rules));
+	DI::pConfig()->set($uid, 'timelinefilter', 'disable', $disable);
 }
 
 function timelinefilter_page_end(&$html)
 {
-    $uid = DI::userSession()->getLocalUserId();
-    if (!$uid || empty($html) || DI::pConfig()->get($uid, 'timelinefilter', 'disable', 1)) {
-        return;
-    }
+	$uid = DI::userSession()->getLocalUserId();
+	if (!$uid || empty($html) || DI::pConfig()->get($uid, 'timelinefilter', 'disable', 1)) {
+		return;
+	}
 
-    $rules_json = DI::pConfig()->get($uid, 'timelinefilter', 'rules', '[]');
-    $rules = json_decode($rules_json, true);
-    if (empty($rules)) {
-        return;
-    }
+	$rules = timelinefilter_get_rules($uid);
+	if (empty($rules)) {
+		return;
+	}
 
-    $hashtag_filters = [];
-    $word_filters = [];
-    $current_time = time();
-    $expired_found = false;
+	$hashtags = [];
+	$words = [];
 
-    foreach ($rules as $rule) {
-        if (isset($rule['expires']) && $rule['expires'] > 0 && $current_time > $rule['expires']) {
-            $expired_found = true;
-            continue;
-        }
+	foreach ($rules as $r) {
+		$kw = mb_strtolower($r['keyword']);
+		if ($r['type'] === 'hashtag') {
+			$hashtags[] = ltrim($kw, '#');
+		} else {
+			$words[] = $kw;
+		}
+	}
 
-        $word = mb_strtolower($rule['keyword']);
-        if ($rule['type'] === 'hashtag') {
-            $hashtag_filters[] = ltrim($word, '#');
-        } else {
-            $word_filters[] = $word;
-        }
-    }
+	if (empty($hashtags) && empty($words)) {
+		return;
+	}
 
-    if ($expired_found) {
-        $clean_rules = array_filter($rules, function($r) use ($current_time) {
-            return !(isset($r['expires']) && $r['expires'] > 0 && $current_time > $r['expires']);
-        });
-        DI::pConfig()->set($uid, 'timelinefilter', 'rules', json_encode(array_values($clean_rules)));
-    }
+	$script = '<script>
+	(function() {
+		"use strict";
 
-    if (empty($hashtag_filters) && empty($word_filters)) {
-        return;
-    }
+		var hashtags = ' . json_encode($hashtags) . ';
+		var words = ' . json_encode($words) . ';
+		var POST_SELECTOR = "article, .thread-wrapper, .wall-item-container";
 
-    $script = '
-    <script>
-    (function() {
-        "use strict";
+		function filterPost(post) {
+			if (!post || post.nodeType !== 1 || post.dataset.tfFiltered) {
+				return;
+			}
 
-        const hashtags = ' . json_encode($hashtag_filters) . ';
-        const words = ' . json_encode($word_filters) . ';
+			post.dataset.tfFiltered = "true";
+			var text = post.textContent.toLowerCase();
 
-        if (hashtags.length === 0 && words.length === 0) {
-            return;
-        }
+			for (var i = 0; i < words.length; i++) {
+				if (text.indexOf(words[i]) !== -1) {
+					post.style.setProperty("display", "none", "important");
+					return;
+				}
+			}
 
-        const POST_SELECTOR = "article, .thread-wrapper, .wall-item-container";
+			if (hashtags.length > 0) {
+				for (var j = 0; j < hashtags.length; j++) {
+					if (text.indexOf("#" + hashtags[j]) !== -1) {
+						post.style.setProperty("display", "none", "important");
+						return;
+					}
+				}
 
-        function filterPost(post) {
-            if (!post || post.nodeType !== Node.ELEMENT_NODE || post.dataset.tfFiltered) {
-                return;
-            }
+				var links = post.querySelectorAll("a[href]");
+				for (var k = 0; k < links.length; k++) {
+					var href = links[k].getAttribute("href").toLowerCase();
+					for (var l = 0; l < hashtags.length; l++) {
+						if (href.indexOf("tag/" + hashtags[l]) !== -1 || href.indexOf("tag=" + hashtags[l]) !== -1) {
+							post.style.setProperty("display", "none", "important");
+							return;
+						}
+					}
+				}
+			}
+		}
 
-            post.dataset.tfFiltered = "true";
-            const postText = post.textContent.toLowerCase();
+		var posts = document.querySelectorAll(POST_SELECTOR);
+		for (var i = 0; i < posts.length; i++) {
+			filterPost(posts[i]);
+		}
 
-            const hasWord = words.some(word => postText.includes(word));
-            if (hasWord) {
-                post.style.setProperty("display", "none", "important");
-                return;
-            }
+		var target = document.getElementById("threads-location") || document.body;
+		var observer = new MutationObserver(function(mutations) {
+			mutations.forEach(function(m) {
+				m.addedNodes.forEach(function(node) {
+					if (node.nodeType !== 1) return;
+					if (node.matches && node.matches(POST_SELECTOR)) {
+						filterPost(node);
+					} else if (node.querySelectorAll) {
+						var subPosts = node.querySelectorAll(POST_SELECTOR);
+						for (var x = 0; x < subPosts.length; x++) {
+							filterPost(subPosts[x]);
+						}
+					}
+				});
+			});
+		});
 
-            if (hashtags.length > 0) {
-                const hasTagInText = hashtags.some(tag => postText.includes("#" + tag));
-                if (hasTagInText) {
-                    post.style.setProperty("display", "none", "important");
-                    return;
-                }
+		observer.observe(target, { childList: true, subtree: true });
+	})();
+	</script>';
 
-                const links = post.querySelectorAll("a[href]");
-                const hasTagInLink = Array.from(links).some(link => {
-                    const href = link.getAttribute("href").toLowerCase();
-                    return hashtags.some(tag => href.includes("tag/" + tag) || href.includes("tag=" + tag));
-                });
+	$html .= $script;
+}
 
-                if (hasTagInLink) {
-                    post.style.setProperty("display", "none", "important");
-                }
-            }
-        }
+/**
+ * Helper to fetch rules and clear expired ones
+ */
+function timelinefilter_get_rules($uid)
+{
+	$json = DI::pConfig()->get($uid, 'timelinefilter', 'rules', '[]');
+	$rules = json_decode($json, true);
+	if (!is_array($rules)) {
+		return [];
+	}
 
-        document.querySelectorAll(POST_SELECTOR).forEach(filterPost);
+	$now = time();
+	$clean = [];
+	$changed = false;
 
-        const targetNode = document.getElementById("threads-location") || document.body;
-        const observer = new MutationObserver((mutations) => {
-            for (const mutation of mutations) {
-                mutation.addedNodes.forEach(node => {
-                    if (node.nodeType !== Node.ELEMENT_NODE) return;
+	foreach ($rules as $r) {
+		if (!empty($r['expires']) && $r['expires'] > 0 && $now > $r['expires']) {
+			$changed = true;
+			continue;
+		}
+		$clean[] = $r;
+	}
 
-                    if (node.matches && node.matches(POST_SELECTOR)) {
-                        filterPost(node);
-                    } else if (node.querySelectorAll) {
-                        node.querySelectorAll(POST_SELECTOR).forEach(filterPost);
-                    }
-                });
-            }
-        });
+	if ($changed) {
+		DI::pConfig()->set($uid, 'timelinefilter', 'rules', json_encode($clean));
+	}
 
-        observer.observe(targetNode, { childList: true, subtree: true });
-    })();
-    </script>
-    ';
-
-    $html .= $script;
+	return $clean;
 }
